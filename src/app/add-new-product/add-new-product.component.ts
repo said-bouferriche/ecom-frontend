5137e7e27ba345e1bec606376fce8c203014904a
unknown <saidbouferriche@gmail.com>
2022-12-20 22:32:32 +0100

    adding drag directive and Authguard authorization to addNewProduct

--- src/app/add-new-product/add-new-product.component.ts
+++ src/app/add-new-product/add-new-product.component.ts
@@ -37,6 +37,7 @@ export class AddNewProductComponent implements OnInit {
       .subscribe(
         (response:Product)=>{
           productForm.reset();
+          this.product.productImages = [];
     },(error:HttpErrorResponse)=>{
           console.log(error);
         }
@@ -80,4 +81,7 @@ export class AddNewProductComponent implements OnInit {
     this.product.productImages.splice(i, 1);
   }
 
+  fileDropped(fileHandle: FileHandle) {
+    this.product.productImages.push(fileHandle);
+  }
 }
